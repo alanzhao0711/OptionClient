9265d277181b79ecfee37e7b57b9335470536647
Alan Zhao <jiahaozhao1@gmail.com>
2023-05-02 12:52:13 -0500

    make it slighty for mobile

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,17 +4,36 @@ import StoreIcon from "@mui/icons-material/Store";
 import RequestPageIcon from "@mui/icons-material/RequestPage";
 import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const handleToggle = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
     <div className="sidebar">
       <div className="top">
         <Link to="/" style={{ textDecoration: "none" }}>
           <img src="images/logo.png" alt="" />
         </Link>
+        <button
+          className="btn btn-primary d-block d-md-none"
+          type="button"
+          onClick={handleToggle}
+          style={{
+            backgroundColor: "#a2d5f2",
+            border: "none",
+            marginLeft: "2rem",
+          }}
+        >
+          <i class="bi bi-list"></i>
+        </button>
       </div>
       <hr />
-      <div className="center">
+      <div className={`center ${isCollapsed ? "collapse" : ""}`}>
         <ul>
           <p className="title">MAIN</p>
           <Link to="/" style={{ textDecoration: "none" }}>
