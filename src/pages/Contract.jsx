60384ee5658975e4c6774e09b9b8dc8beb121f0c
Alan Zhao <jiahaozhao1@gmail.com>
2023-05-10 10:42:04 -0500

    make sure data is updated even in contracts

--- src/pages/Contract.jsx
+++ src/pages/Contract.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Contract.scss";
 import Sidebar from "../components/sidebar/Sidebar";
 import getDate from "../components/dashboard/GetDate";
 import AllOptions from "../components/account/AllOptions";
 
 const Contract = () => {
+  useEffect(() => {
+    socket.emit("dash");
+    const interval = setInterval(() => {
+      socket.emit("dash");
+    }, 300000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="home">
       <Sidebar />
